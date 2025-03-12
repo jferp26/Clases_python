20fcf24540851a6a19458717ae7d87c5199580e8
jferp26 <jferp26@gmail.com>
2025-03-12 14:00:31 +0100

    corregido 12 de marzo

--- agenda_json.py
+++ agenda_json.py
@@ -21,10 +21,47 @@ class Agenda:
                 print(f"Nombre: {nombre}")
                 print(f"telefono: {info['telefono']}")
                 print(f"Email: {info['email']}\n")
-        
         else:
             print("la agenda esta vacia")
+    
+    def buscarcontactos(self):
+        nombre= str.upper(input("introduzca el nombre a buscar: "))
+        if nombre in self.contactos.items():
+                info = self.contactos[nombre]
+                print(f"Nombre: {nombre}")
+                print(f"telefono: {info['telefono']}")
+                print(f"Email: {info['email']}\n")
+        
+        else:
+            print("contacto no encontrado")
 
+    def borrarcontacto(self):
+        nombre= str.title(input("introduzca el nombre a borrar: "))
+        if nombre in self.contactos.items():
+                self.contactos.pop(nombre)
+              #  del self.contactos[nombre]
+                print(f"el contaco {nombre} se ha borrado")
+        else:
+            print("contacto no encontrado")
+    
+
+    def añadircontacto(self):
+        nombre = input("Introduzca el nombre del contacto: ")
+        telefono = input("Introduzca el número de teléfono: ")
+        email  = input("Introduzca el número de Email: ")
+        self.contactos[nombre] = {"telefono": telefono , "email": email}
+        print(f"Se ha añadido el contacto {nombre} con el número {telefono} y el email{email}.")
+    
+    def guardaragenda(self):
+         with open(self.archivo,"w") as f:
+              json.dump(self.contactos, f, indent=4)
+              
+#__name__ nombre del archivo que no se ejecuta el primero
+#__main__ archivo que se ejecuta en primer plano
+def menu():
+    pass
+if __name__=="__main__":
+     menu()
         
 Agenda_test = Agenda()
 Agenda_test.mostraragenda()
